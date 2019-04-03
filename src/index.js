e5b861fc21e12ee368fef7ee8b38c578f14e31c5
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2019-04-03 10:29:51 -0500

    probando primera pagina

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
+import BadgeNew from './pages/BadgeNew';
+const container = document.getElementById('root');
+ReactDOM.render(<BadgeNew/>, container);
 
-ReactDOM.render(<App firstOption = "Ingresar nombre del cliente:" secondOption = "Ingresar número de mesa"/>, document.getElementById('root'));
+
+
+
+// ReactDOM.render(<App firstOption = "Ingresar nombre del cliente:" secondOption = "Ingresar número de mesa"/>, container);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/pages/BadgeNew.js
@@ -0,0 +1,9 @@
+import React from 'react';
+class BadgeNew extends React.Component {
+    render() {
+        return (
+            <h1>Badge New</h1>
+        )
+    }
+}
+export default BadgeNew;
