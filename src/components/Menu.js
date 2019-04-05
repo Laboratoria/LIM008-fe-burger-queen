1a8a8536dee4680a5f4c71e014291cbbdb359cc1
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2019-04-05 09:48:03 -0500

    añadiendo json de prueba

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,13 +1,42 @@
 import React from 'react';
+
 class Menu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
+
+  componentDidMount() {
+    fetch('')
+  }
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     menu: menu
+  //   }
+  // }
+
+  // render() {
+  //   const menu = this.state.menu.map((item, i) => {
+  //     return (
+  //       <div>
+  //         {item.title}
+  //       </div>
+  //     )
+  //   })
+
+  //   return (
+  //     <div>
+  //       {menu}
+  //     </div>  
+  //     )
+  // }
   render() {
-    return (
-      <div>
-        <button>HAMBURGUESAS</button>
-        <button>ACOMPAÑAMIENTOS</button>
-        <button>BEBIDAS</button>
-        <button>ADICIONAL</button>
-      </div>
+    return(
+      <div></div>
     )
   }
 }
