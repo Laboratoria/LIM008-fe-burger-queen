369b1274a01e778479d9f9bacb4110a1e294aad8
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2019-04-05 14:35:05 -0500

    modificando json

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,63 +3,63 @@ import menu from '../data/menu'
 
 class Menu extends React.Component {
 
-  constructor() {
-    super();
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     menu: menu
+  //   }
+  // }
+
+  // render() {
+  //   const menu = this.state.menu.map((item, i) => {
+  //     return (
+  //       <div>
+  //         <button>{item.title} - {item.value}</button>
+  //       </div>
+  //     )
+  //   })
+
+  //   return (
+  //     <div>
+  //       {menu}
+  //     </div>  
+  //     )
+  // }
+    constructor(props) {
+    super(props);
     this.state = {
-      menu: menu
+      items: [],
+      isLoaded: false,
     }
   }
 
+  componentDidMount() {
+    fetch('https://raw.githubusercontent.com/mahaliroblesarbieto/LIM008-fe-burger-queen/developer/src/data/menu.json')
+    .then(res => res.json())
+    .then(json => {
+      this.setState({
+        isLoaded: true,
+        items:json,
+      })
+    })
+  }
   render() {
-    const menu = this.state.menu.menu.map((item, i) => {
-      return (
+    let{isLoaded, items} = this.state;
+    if(!isLoaded) {
+      return <div>Loading...</div>
+    }
+    else {
+      return(
         <div>
-          <button>{item.title} - {item.value}</button>
+          <ul>
+            {items.map(item => (
+            <li key={item.title}>
+            {item.value}</li>))}
+          </ul>
         </div>
       )
-    })
-
-    return (
-      <div>
-        {menu}
-      </div>  
-      )
-  }
-    // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     items: [],
-  //     isLoaded: false,
-  //   }
-  // }
-
-  // componentDidMount() {
-  //   fetch('https://raw.githubusercontent.com/mahaliroblesarbieto/LIM008-fe-burger-queen/developer/src/data/menu.json')
-  //   .then(res => res.json())
-  //   .then(json => {
-  //     this.setState({
-  //       isLoaded: true,
-  //       items:json,
-  //     })
-  //   })
-  // }
-  // render() {
-  //   let{isLoaded, items} = this.state;
-  //   if(!isLoaded) {
-  //     return <div>Loading...</div>
-  //   }
-  //   else {
-  //     return(
-  //       <div>
-  //         <ul>
-  //           {items.map(item => (
-  //           <li key={item.title}>
-  //           {item.value}</li>))}
-  //         </ul>
-  //       </div>
-  //     )
-  //   }
+    }
    
-  // }
+  }
 }
 export default Menu;
