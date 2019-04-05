3838ae023f45bd6adc09d44736cacc4c92e04d1b
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2019-04-04 21:44:38 -0500

    añadiendo json

---
+++ src/components/Options.js
@@ -0,0 +1,9 @@
+// import React from 'react';
+
+// const Options = (props) => {
+//     return (<li>
+//         <span>{props.name}{props.value}</span>
+//     </li>)
+// }
+
+// export default Options;
--- src/components/Orden.js
+++ src/components/Orden.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import './styles/Navbar.css';
 import firebase from '../firestore.js'
+// import Options from './Options.js'
 class Orden extends React.Component {
+  // state = {
+  //   menu: [
+  //     {name: 'Hamburguesa simple de res', value: 10},
+  //     {name: 'Hamburguesa simple de pollo', value: 10},
+  //     {name: 'Hamburguesa simple vegetariana', value: 10},
+  //     {name: 'Hamburguesa doble de res', value: 15},
+  //     {name: 'Hamburguesa doble de pollo', value: 15},
+  //     {name: 'Hamburguesa doble vegetariana', value: 15},
+  //   ]
+  // }
     addUser = e => { 
         e.preventDefault ();
         const db = firebase.firestore();
@@ -50,6 +61,15 @@ class Orden extends React.Component {
               name = "table_number"
               onChange={this.updateInput}
               value={this.state.table_number}></input>
+              {/* <div>
+                <ul>
+                  {
+                    this.state.menu.map((menu) => {
+                      return (<Options name = {menu.name}>{menu.value}</Options>)
+                    })
+                  }
+                </ul>
+              </div> */}
               <button type = "submit">Enviar a cocina</button>
             </div>
           </form>
