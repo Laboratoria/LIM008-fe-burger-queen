34a8a3d6feba08fd9523bd1b9650153614222c4e
jenifercoila <jeniferfcr@gmail.com>
2019-04-23 14:40:56 -0500

    Aplico flexbox a los botones

--- build/burger-queen/src/environments/environment.prod.ts
+++ build/burger-queen/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBNWayBfnYCgzlgtDsYqd9BQS5ZEZEhlYU",
+    authDomain: "bqproject-8fc8f.firebaseapp.com",
+    databaseURL: "https://bqproject-8fc8f.firebaseio.com",
+    projectId: "bqproject-8fc8f",
+    storageBucket: "bqproject-8fc8f.appspot.com",
+    messagingSenderId: "650449756487"
+  },
 };
