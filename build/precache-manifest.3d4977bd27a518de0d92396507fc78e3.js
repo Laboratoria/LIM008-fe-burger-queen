3bd915e7a8d96f6dccf34655b4b7854ba012b86a
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2019-04-23 06:53:44 -0500

    modificando version

--- build/precache-manifest.3d4977bd27a518de0d92396507fc78e3.js
+++ build/precache-manifest.3d4977bd27a518de0d92396507fc78e3.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/LIM008-fe-burger-queen/static/js/runtime~main.10776b7b.js"
   },
   {
-    "revision": "066d0b813a07ddfd9be2",
-    "url": "/LIM008-fe-burger-queen/static/js/main.6f01cce1.chunk.js"
+    "revision": "a39fd629ba29ba5681da",
+    "url": "/LIM008-fe-burger-queen/static/js/main.7c78c2ef.chunk.js"
   },
   {
     "revision": "5f7d52c676384ac59070",
     "url": "/LIM008-fe-burger-queen/static/js/2.fbb16d12.chunk.js"
   },
   {
-    "revision": "066d0b813a07ddfd9be2",
+    "revision": "a39fd629ba29ba5681da",
     "url": "/LIM008-fe-burger-queen/static/css/main.53b73bcb.chunk.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/LIM008-fe-burger-queen/static/css/2.16434e7e.chunk.css"
   },
   {
-    "revision": "b3f82f96bc0bab7f24c07e25dbcc6d17",
+    "revision": "3ba0ae09137681e99b42cd00aeb621bb",
     "url": "/LIM008-fe-burger-queen/index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/LIM008-fe-burger-queen/precache-manifest.450d04716c90953f45b1349cece9bc61.js"
+  "/LIM008-fe-burger-queen/precache-manifest.3d4977bd27a518de0d92396507fc78e3.js"
 );
 
 workbox.clientsClaim();
