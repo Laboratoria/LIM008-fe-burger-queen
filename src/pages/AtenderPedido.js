72da75035514faab9e9e9a4904e4e7b3dda1450f
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2019-05-03 06:10:24 -0500

    mostrando data

--- src/pages/AtenderPedido.js
+++ src/pages/AtenderPedido.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import firebase from 'firebase';
 
 export default function AtenderPedido() {
+  const { error, loading, value } = useCollection(
+    firebase.firestore().collection('users'),
+  );
   return (
-    <p>lo lograste</p>
+    <div className="container center margin-option">
+      <div className="row weigth font-mayor">
+        <div className="col-3">CLIENTE</div>
+        <div className="col-3">PRODUCTO</div>
+        <div className="col-3">CANTIDAD</div>
+        <div className="col-3">ENVIAR</div>
+      </div>
+      {error && <strong>Error: {error}</strong>}
+      {loading && <span>Collection: Loading...</span>}
+      {value && (
+        <span>
+          {value.docs.map(doc => (
+            // console.log(doc.data().orden[0].count)
+            <div className="row margin-option font-menor section" data-testid="item" key={doc.id}>
+              <div className="col-3">{doc.data().name}</div>
+              <div className="col-3 center">{doc.data().orden.map(item => (<div>{item.name}</div>))}</div>
+              <div className="col-3 center">{doc.data().orden.map(item => (<div>{item.count}</div>))}</div>
+              <div className="col-3 center">
+                <button type="button" className="button muted-button button-delete margin-button-count-substr"><i className='fas fa-share'></i></button>
+              </div>
+            </div>
+            // <React.Fragment key={doc.id}>
+            //   {JSON.stringify(doc.data())},{' '}
+            // </React.Fragment>
+          ))}
+        </span>
+      )}
+    </div>
+
   );
 }
